e417209f900c37a1cfead1ced4affedc75f1be83
AKABANE Kazuaki <kurikuto@local>
2023-11-09 11:18:22 +0900

    schema

--- api/main.py
+++ api/main.py
@@ -6,6 +6,6 @@ app.include_router(task.router)
 app.include_router(done.router)
 
 
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world!"}
+# @app.get("/hello")
+# async def hello():
+#     return {"message": "hello world!"}
