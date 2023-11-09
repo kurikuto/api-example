53740e479d4ff62bf26a64a4f82580e720c39e72
AKABANE Kazuaki <kurikuto@local>
2023-11-09 22:39:26 +0900

    update

--- api/cruds/task.py
+++ api/cruds/task.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from sqlalchemy import select
 from sqlalchemy.engine import Result 
 import api.models.task as task_model
@@ -7,6 +7,7 @@ import api.schemas.task as task_schema
 
 
 async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+    """タスク一覧を取得する"""
     result: Result = await (
         db.execute(
             select(
@@ -22,8 +23,29 @@ async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
 async def create_task(
     db: AsyncSession, task_create: task_schema.TaskCreate
 ) -> task_model.Task:
+    """新規タスクを作成する"""
     task = task_model.Task(**task_create.dict())
     db.add(task)
     await db.commit()
     await db.refresh(task)
     return task
+
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
+    """idに紐づくタスクを取得する"""
+    result: Result = await db.execute(
+        select(task_model.Task).filter(task_model.Task.id == task_id)
+    )
+    task: Optional[Tuple[task_model.Task]] = result.first()
+    return task[0] if task is not None else None
+
+
+async def update_task(
+    db: AsyncSession, task_create: task_schema.TaskCreate, original: task_model.Task
+) -> task_model.Task:
+    """タスクを更新する"""
+    original.title = task_create.title
+    db.add(original)
+    await db.commit()
+    await db.refresh(original)
+    return original
